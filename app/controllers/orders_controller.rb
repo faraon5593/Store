fc7135982cca63496e11708ab2c3fba2c971426c
faraon5593 <faraon5593@gmail.com>
2015-02-04 00:10:31 +0100

    autentykacja przy orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,8 @@
 class OrdersController < ApplicationController
 
   before_action :set_order, only: [:show, :edit, :update, :destroy]
+    before_action :authenticate_user!, only: [:index,:show, :edit, :update, :destroy]
+
   layout "order"
 
   # GET /orders
